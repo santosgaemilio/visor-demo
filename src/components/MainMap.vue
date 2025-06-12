4d4d2f49116a55d30b9afd34bb7e6e420d6a41be
santosgaemilio <santosgaemilio@gmail.com>
2025-06-11 23:15:35 -0600

    fix: cambio centro

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -4,12 +4,20 @@ import 'leaflet/dist/leaflet.css'
 import { ref, onMounted } from 'vue'
 
 const mapDiv = ref('map')
+
+// Todo lo que tenga que ver con el mapa se tiene que hacer dentro del onMounted()
 onMounted(() => {
   //25.70630097838529, -100.30974546183137 Centro de zona metropolitana
-  const map = L.map('map', { zoomControl: false }).setView([25.706, -100.309], 12)
+  const map = L.map('map', { zoomControl: false }).setView([25.6768, -100.214706], 13)
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map)
+  void L.circle([25.676892598598613, -100.2147064790041], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500,
   }).addTo(map)
 })
 </script>
